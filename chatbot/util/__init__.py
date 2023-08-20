1227f2e17083fdb71cc2ee057bb1d84187401a17
DDGRCF <raochaofan1970@gmail.com>
2023-08-20 09:59:29 +0800

    feat: add jupyter client

--- chatbot/util/__init__.py
+++ chatbot/util/__init__.py
@@ -1,5 +1,6 @@
 from .file_util import *
 from .model_util import *
 from .config_util import *
+from .jupyter_util import *
 
-_all_ = ['parse_files', 'Config', 'LLaMA2', 'ModelType', 'BackendType', 'ModelFactory', 'QWen']
+_all_ = ['parse_files', 'Config', 'LLaMA2', 'ModelType', 'BackendType', 'ModelFactory', 'QWen', 'JupyterNotebook']
