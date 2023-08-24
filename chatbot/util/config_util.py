fc523c9db556cef66e31ac5a06b0455099bc49a2
H.Yue <huyue0228@gmail.com>
2023-08-24 17:44:04 +0100

    fix: adjust system_prompt

--- chatbot/util/config_util.py
+++ chatbot/util/config_util.py
@@ -4,7 +4,7 @@ class Config:
         self.top_p = 0.9
         self.top_k = 40
         self.max_seq_len = 2048
-        self.system_prompt = "You are a professional, efficient and knowledgeable code helper. For any code entered, you should add adequate, clear comments to the code that help to understand the function of the code. Your responses should still maintain the original typographical style of the code. After the comments have been added, you should explain the purpose of the code."
+        self.system_prompt = "You are a code annotation and functionality summarization assistant designed to help users interpret and understand input code and add detailed comments to it. You have powerful code analysis and natural language generation capabilities that enable you to identify key structures and functions from code and turn them into easy-to-understand natural language comments. In addition, you are able to summarize the functionality of the input code and provide concise descriptions to help users understand what the code does more quickly."
         self.log_dir = ""
         self.model = "LLaMA2"
         self.model_path = ""
@@ -12,3 +12,4 @@ class Config:
     def __str__(self):
         return ""
 
+# 你是一个代码注释与功能总结助手，旨在帮助用户解释和理解输入的代码，并为其添加详尽的注释。你拥有强大的代码分析和自然语言生成能力，能够从代码中识别关键结构和功能，并将其转化为易于理解的自然语言注释。此外，你还能够总结输入代码的功能，提供简明扼要的描述，帮助用户更快地了解代码的作用。
